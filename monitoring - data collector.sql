0d8f5d4ccc533376d26d7465c9168e81ff44f3f9
Natalia Menk <91092841+NatMenk@users.noreply.github.com>
2021-09-28 11:27:20 -0600

    Add files via upload

---
+++ monitoring - data collector.sql
@@ -0,0 +1,43 @@
+--Create a custom data collector set
+DECLARE @collection_set_id_1 INT
+DECLARE @collection_set_uid_2 uniqueidentifier
+
+--create the main data collector set object
+--step 1
+EXEC msdb.dbo.sp_syscollector_create_collection_set
+	@name=N'Cache Usage Report', --name of the data collector set
+	@collection_mode=0, --0=cached 1 = non-cached
+	@description=N'Cache Usage Report', 
+	@logging_level=1,
+	@days_until_expiration=14, --data rentention in the MDW
+	@schedule_name=N'CollectorSchedule_Every_6h', 
+	@collection_set_id=@collection_set_id_1 OUTPUT, 
+	@collection_set_uid=@collection_set_uid_2 OUTPUT
+
+--step 2
+DECLARE @collector_type_uid_3 uniqueidentifier
+SELECT @collector_type_uid_3 = collector_type_uid fROM [msdb].[dbo].[syscollector_collector_types] 
+WHERE name = N'Generic T-SQL Query Collector Type';
+
+--step 3
+DECLARE @collection_item_id_4 INT
+EXEC msdb.dbo.sp_syscollector_create_collection_item
+	@name=N'Cache Usage Report', 
+	@parameters=N'<ns:TSQLQueryCollector xmlns:ns="DataCollectorType"><Query><Value>
+
+        SELECT objtype AS ''Cached Object Type'',
+        COUNT(*) as ''Number of Plans'',
+        SUM(cast(size_in_bytes as BIGINT))/1048576 as ''Plan Cache SIze (MB)'',
+        avg(usecounts) as ''Avg Use Counts''
+        from sys.dm_exec_cached_plans
+        group by objtype
+        order by objtype
+         
+        </Value><OutputTable>Cache_Usage_Report</OutputTable></Query></ns:TSQLQueryCollector>',
+	@collection_item_id=@collection_item_id_4 OUTPUT, 
+	@frequency=15, --how often this collection item will run
+	@collection_set_id=@collection_set_id_1, 
+	@collector_type_uid=@collector_type_uid_3
+
+
+
